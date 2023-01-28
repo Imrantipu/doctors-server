a839ad4516e83878a0652e9e87766ebcda73b795
Imran <imrantipu90@yahoo.com>
2023-01-28 11:42:12 +0100

    mongodb database connection with server

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 5000;
@@ -7,6 +8,13 @@ const port = process.env.PORT || 5000;
 // middelware
 app.use(cors())
 app.use(express.json());
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.z2xprxb.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
 app.get('/', (req, res) => {

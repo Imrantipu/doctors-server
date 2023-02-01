2e7d54e0521432f65a652ad6592f26be39f44493
Imran <imrantipu90@yahoo.com>
2023-02-01 19:06:58 +0100

    verify jeson web token to get token from client and used in bookings

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const app = express()
@@ -9,12 +10,25 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json());
 
-
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.z2xprxb.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send("unauthorized access");
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
 
 
 async function run() {
@@ -39,8 +53,13 @@ async function run() {
         res.send(options);
       })
 
-      app.get("/bookings", async (req,res)=>{
+      app.get("/bookings", verifyJWT, async (req,res)=>{
         const email = req.query.email;
+        const decodedEmail = req.decoded.email;
+
+        if (email !== decodedEmail) {
+          return res.status(403).send({ message: "forbidden access" });
+        }
         const query = {email:email};
         const bookings = await bookingsCollection.find(query).toArray();
         res.send(bookings);

e840717c18cac084439f56a29f41edc00e7352ca
Imran <imrantipu90@yahoo.com>
2023-02-02 19:03:15 +0100

    email used to checks admin not the id

--- index.js
+++ index.js
@@ -94,9 +94,9 @@ async function run() {
       res.send(result);
     });
 
-    app.get('/users/admin/:id', async (req,res) => {
-         const id = req.params.id;
-         const query = { _id: ObjectId(id)};
+    app.get('/users/admin/:email', async (req,res) => {
+         const email = req.params.email;
+         const query = { email };
          const user = await usersCollection.findOne(query);
          res.send({isAdmin: user?.role === 'admin'});
     })

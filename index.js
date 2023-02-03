9f10c6fbc34d5b4b939d64aa267a9e6825f16ed2
Imran <imrantipu90@yahoo.com>
2023-02-03 20:45:35 +0100

    single booking with id for payment

--- index.js
+++ index.js
@@ -75,6 +75,12 @@ async function run() {
         const bookings = await bookingsCollection.find(query).toArray();
         res.send(bookings);
       });
+      app.get("/bookings/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const booking = await bookingsCollection.findOne(query);
+        res.send(booking);
+      });
 
       app.post('/bookings', async (req, res) => {
       const booking = req.body;
@@ -163,18 +169,18 @@ async function run() {
       }
       res.status(403).send({ accessToken: "" });
     });
-// update price to all appointmentCollections 
-    app.get('/addPrice', async (req, res) => {
-      const filter = {}
-      const options = { upsert: true }
-      const updatedDoc = {
-          $set: {
-              price: 99
-          }
-      }
-      const result = await appointmentOptionsCollection.updateMany(filter, updatedDoc, options);
-      res.send(result);
-  })
+// // update price to all appointmentCollections 
+//     app.get('/addPrice', async (req, res) => {
+//       const filter = {}
+//       const options = { upsert: true }
+//       const updatedDoc = {
+//           $set: {
+//               price: 99
+//           }
+//       }
+//       const result = await appointmentOptionsCollection.updateMany(filter, updatedDoc, options);
+//       res.send(result);
+//   })
     
   } finally {
    

adcea1c5917bc2aad560af4550ad5bb2454ae4d7
Imran <imrantipu90@yahoo.com>
2023-02-03 17:18:37 +0100

    verify admin middleware created

--- index.js
+++ index.js
@@ -38,6 +38,17 @@ async function run() {
     const usersCollection = client.db("dentalService").collection("users");
     const doctorsCollection = client.db("dentalService").collection("doctor");
 
+    const verifyAdmin = async (req, res, next) => {
+      const decodedEmail = req.decoded.email;
+      const query = { email: decodedEmail };
+      const user = await usersCollection.findOne(query);
+
+      if (user?.role !== "admin") {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      next();
+    };
+
     app.get('/appointmentOptions', async (req, res) => {
       const date = req.query.date;
         const query = {};
